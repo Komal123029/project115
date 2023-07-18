b07ccb00960b839ffe22fcd8b5d8d31991798182
Komal123029 <105293426+Komal123029@users.noreply.github.com>
2023-07-18 20:48:33 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+a = int(input("please enter a number : "))
+b = int(input("please enter a number : "))
+
+if(b > a ):
+    print("b is greater then a")
+elif a > b:
+    print("a is greater then b") 
+
+# firstly i changed the variable to input 
+# then i had  put the bracket of if condition 
+# then in elif condition i have changed == sign to > sign      
